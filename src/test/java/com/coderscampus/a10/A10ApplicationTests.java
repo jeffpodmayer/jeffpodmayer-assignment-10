fd76c2dda872034f28a77e9ff9389a861a7853f0
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2024-01-10 16:09:23 -0800

    Initial commit of project setup

---
+++ src/test/java/com/coderscampus/a10/A10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.a10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
