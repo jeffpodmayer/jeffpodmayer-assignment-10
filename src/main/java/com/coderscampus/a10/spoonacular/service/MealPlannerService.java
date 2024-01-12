d23c1de9b6e0b0869b80e6ebe1ac42986cce517e
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2024-01-12 12:56:10 -0800

    testes requestParams

--- src/main/java/com/coderscampus/a10/spoonacular/service/MealPlannerService.java
+++ src/main/java/com/coderscampus/a10/spoonacular/service/MealPlannerService.java
@@ -20,16 +20,18 @@ public class MealPlannerService {
 	@Value("${spoonacular.urls.mealplan}")
 	private String mealPlan;
 	
+	@Value("${spoonacular.api.key}")
+	private String apiKey;
 	
 	RestTemplate rt = new RestTemplate(); 
 	
 	public ResponseEntity<DayResponse> fetchSpoonacularDataForDay() {
 		URI uri = UriComponentsBuilder.fromHttpUrl(baseUrl + mealPlan)
+							.queryParam("apiKey", apiKey)
 							.queryParam("timeFrame", "day")
-							.queryParam("targetCalories", "2500")
+							.queryParam("targetCalories", "1500")
 							.queryParam("diet", "vegetarian")
-							.queryParam("exclude", "")
-							.queryParam("apiKey", "52b2023f776e49889eebc7271c73ce40")
+							.queryParam("exclude", "shellfish")	
 							.build()
 							.toUri();
 							
@@ -41,13 +43,13 @@ public class MealPlannerService {
 	
 	public ResponseEntity<WeekResponse> fetchSpoonacularDataForWeek() {
 		URI uri = UriComponentsBuilder.fromHttpUrl(baseUrl + mealPlan)
-				.queryParam("timeFrame", "week")
-				.queryParam("targetCalories", "3000")
-				.queryParam("diet", "ovovegetarian")
-				.queryParam("exclude", "shellfish")
-				.queryParam("apiKey", "52b2023f776e49889eebc7271c73ce40")
-				.build()
-				.toUri();
+							.queryParam("apiKey", apiKey)
+							.queryParam("timeFrame", "week")
+							.queryParam("targetCalories", "2000")
+							.queryParam("diet", "vegetarian")
+							.queryParam("exclude", "olives")
+							.build()
+							.toUri();
 				
 		ResponseEntity<WeekResponse> weekResponse = rt.getForEntity(uri, WeekResponse.class);
 		return weekResponse;
